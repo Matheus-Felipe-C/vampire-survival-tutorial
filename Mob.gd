4b2546b1006db37822e3208fe65ead640f5b1cf9
Matheus Felipe <theusf40@hotmail.com>
2024-05-07 16:45:58 -0300

    Added enemies spawning outside of the player view

--- Mob.gd
+++ Mob.gd
@@ -5,22 +5,22 @@ var health = 3
 @onready var player = get_node("/root/Game/Player")
 
 func _ready():
-    %Slime.play_walk()
+	%Slime.play_walk()
 
 func _physics_process(delta):
-    var direction = global_position.direction_to(player.global_position)
-    velocity = direction * 300.0
-    move_and_slide()
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * 300.0
+	move_and_slide()
 
 func take_damage():
-    health -= 1
-    $Slime.play_hurt()
+	health -= 1
+	$Slime.play_hurt()
 
-    if health == 0:
-        queue_free()
+	if health == 0:
+		queue_free()
 
-        const SMOKE_SCENE = preload("res://smoke_explosion/smoke_explosion.tscn")
+		const SMOKE_SCENE = preload("res://smoke_explosion/smoke_explosion.tscn")
 
-        var smoke = SMOKE_SCENE.instantiate()
-        get_parent().add_child(smoke)
-        smoke.global_position = global_position
+		var smoke = SMOKE_SCENE.instantiate()
+		get_parent().add_child(smoke)
+		smoke.global_position = global_position
